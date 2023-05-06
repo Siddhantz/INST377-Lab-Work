6a34d5805f30ae344542753aef71dc7860550ad3
Siddhantz <stiwar3@terpmail.umd.edu>
2023-05-05 22:56:06 -0400

    Update script.js
    Finished script.js for lab 7

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -45,10 +45,15 @@ function getRandomIntInclusive(min, max) {
     const filterButton = document.querySelector('#filter');
     const loadDataButton = document.querySelector('#data_load');
     const generateListButton = document.querySelector('#generate');
+    const textField = document.querySelector('#resto');
     
     const loadAnimation = document.querySelector('#data_load_animation');
     loadAnimation.style.display = 'none';
+    generateListButton.classList.add('hidden');
   
+
+    let storedList = [];
+    
     let currentList = []; // this is "scoped" to the main event function
     
     /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
@@ -62,9 +67,12 @@ function getRandomIntInclusive(min, max) {
       const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
   
       // This changes the response from the GET into data we can use - an "object"
-      currentList = await results.json();
+      storedList = await results.json();
+      if (storedList.length > 0){
+        generateListButton.classList.remove('hidden');
+    }
       loadAnimation.style.display = 'none';
-      console.table(currentList);
+      console.table(storedList);
     });
   
     filterButton.addEventListener('click', (event)=> {
@@ -82,9 +90,16 @@ function getRandomIntInclusive(min, max) {
   
     generateListButton.addEventListener('click', (event)=> {
       console.log('generate new list');
-      const restaurantsList = cutRestaurantList(currentList);
-      console.log(restaurantsList);
-      injectHTML(restaurantsList);
+      currentList = cutRestaurantList(storedList);
+      console.log(currentList);
+      injectHTML(currentList);
+    })
+
+    textField.addEventListener('input', (event) => {
+        console.log('input', event.target.value);
+        const newList = filterList(currentList, event.target.value);
+        console.log(newList);
+        injectHTML(newList);
     })
   }
   
